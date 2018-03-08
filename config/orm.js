ef8adfc2ea9540b1880fc45c95e4b6704309d042
Khamel <khamel.mumford@gmail.com>
2018-03-07 23:38:44 -0600

    testing orm

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,8 @@
 var connection = require('./connection')
 
-//helper function
-function printQuestionMarks(num) {
-    var arr = [];
-
-    for (var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-
-    return arr.toString();
-}
-
 var orm = {
     selectAll: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
+        var queryString = "SELECT * FROM burgers;"
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
@@ -23,18 +12,12 @@ var orm = {
 
     },
     insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-
-        console.log(queryString);
-
-        connection.query(queryString, vals, function(err, result) {
+        var queryString = `INSERT INTO ${table} SET ? ?`;
+        connection.query(queryString, function(err, result) {
+            {
+                cols: vals
+            }
+            console.log(`${cols} ${vals}`);
             if (err) {
                 throw err;
             }
