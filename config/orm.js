5f1e6aabe8a3709fa1f11ca4a1e2793f724a64f6
Khamel <khamel.mumford@gmail.com>
2018-03-12 18:08:43 -0500

    removing delete route to check heroku

--- config/orm.js
+++ config/orm.js
@@ -10,33 +10,24 @@ var Burger = function(id, name, devoured) {
 // gather all records
 Burger.getAll = function(cb) {
         // request all records from the database
-        connection.query("SELECT * FROM BURGERS;", cb);
+        connection.query("SELECT * FROM BURGER;", cb);
     }
     // We don't need an object to search, so we'll 
     // add it directly to the function
     // criteria = { "name" : "sarah"} or {"id": 1} for example
 Burger.find = function(criteria, cb) {
-        connection.query("SELECT * FROM BURGERS WHERE ?", criteria, cb);
+        connection.query("SELECT * FROM BURGER WHERE ?", criteria, cb);
     }
     // We add the method to the prototype
     // since we are having an object to save
     // create a record
 Burger.prototype.create = function(cb) {
         var burgerData = {
-            burger_name: this.burger_name,
+            name: this.name,
             devoured: this.devoured
         };
-        connection.query("INSERT INTO BURGERS SET ?", burgerData, cb);
+        connection.query("INSERT INTO BURGER SET ?", burgerData, cb);
     }
     // TODO update a record
-Burger.update = function(criteria, cb) {
-        connection.query("UPDATE BURGERS SET ? WHERE ?", )
-    }
     // TODO delete a record
-Burger.delete = function(criteria, cb) {
-    var BurgerId = {
-        id: this.id
-    };
-    connection.query("DELETE FROM BURGERS WHERE ?", BurgerId, cb)
-}
 module.exports = Burger;
