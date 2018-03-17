f9e5ea066086fab27e42c43734b0e63e91547229
Khamel <khamel.mumford@gmail.com>
2018-03-17 14:11:07 -0500

    change DB over to jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 var mysql = require("mysql");
 var connection;
-
 if (process.env.JAWSDB_URL) {
+
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "b!gPh@rma23",
-        database: "burgers_db"
+        host: "v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "nykqfbqkees608ry",
+        password: "jxcrl1duudsqk7xp",
+        database: "k9rs1nvcj0fap6sn"
     });
+    // connection = mysql.createConnection({
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "b!gPh@rma23",
+    //     database: "burgers_db"
+    // });
     // {
     //     if (err) throw err;
     //     //  ();
