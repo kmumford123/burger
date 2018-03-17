ec082d2d34d89f321a94a50e56d684e5dfcb20cb
Khamel <khamel.mumford@gmail.com>
2018-03-17 14:31:51 -0500

    new heroku app

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
 var mysql = require("mysql");
 var connection;
-// if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
 
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-connection = mysql.createConnection({
-    host: "v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "nykqfbqkees608ry",
-    password: "jxcrl1duudsqk7xp",
-    database: "k9rs1nvcj0fap6sn"
-});
-// connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "b!gPh@rma23",
-//     database: "burgers_db"
-// });
-// {
-//     if (err) throw err;
-//     //  ();
-// });
-// };
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "quzxztpvciundcuq",
+        password: "hcuy0zs3wn2hzfkj",
+        database: "r9qp46djxzdwrll7"
+    });
+    // connection = mysql.createConnection({
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "b!gPh@rma23",
+    //     database: "burgers_db"
+    // });
+    // {
+    //     if (err) throw err;
+    //     //  ();
+    // });
+};
 connection.connect()
 
 module.exports = connection
